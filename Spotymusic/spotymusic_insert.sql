767815b417c06518214f43dd0269c67d2d72a716
lucascustodioCATOLICA <142275808+lucascustodioCATOLICA@users.noreply.github.com>
2024-09-25 19:28:48 -0300

    feat: add genero table inserts

--- Spotymusic/spotymusic_insert.sql
+++ Spotymusic/spotymusic_insert.sql
@@ -1,1 +1,13 @@
-
+insert into genero values
+(null, 'Pop'),
+(null, 'Rock'),
+(null, 'Rap'),
+(null, 'Reggae'),
+(null, 'Electronic'),
+(null, 'Funk'),
+(null, 'Heavy Metal'),
+(null, 'Punk'),
+(null, 'Indie'),
+(null, 'MPB'),
+(null, 'Jazz'),
+(null, 'Soul');
